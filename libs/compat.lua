978a97e69a751e20befdae1a4852cfaabe20d199
Tohru~ (トール) <admin@ikaros.pw>
2022-03-29 21:32:35 -0400

    test

---
+++ libs/compat.lua
@@ -0,0 +1,46 @@
+-- Standardize function names + load in some other things
+
+local storage = import('storage')
+
+local globals = getgenv()
+local connections = {}
+
+local compat = {}
+
+compat.request = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or request
+
+compat.queueTeleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+
+compat.setclipboard = setclipboard or toclipboard or set_clipboard
+
+compat.services = setmetatable({}, {
+    __index = function(_, k)
+        return game:GetService(k)
+    end
+})
+
+compat.parentGui = function(gui)
+    local protect_gui = not is_sirhurt_closure and syn and syn.protect_gui
+    local hiddenUI = get_hidden_gui or gethui
+    if protect_gui then
+        protect_gui(gui)
+    elseif hiddenUI then
+        gui.Parent = hiddenUI()
+        return
+    end
+    gui.Parent = game:GetService('CoreGui')
+    return gui
+end
+
+compat.connect = function(signal, fn)
+    local connection = signal:Connect(fn)
+    local position = #connections + 1
+    connections[position] = connection
+    return connection, position
+end
+
+for i, v in pairs(compat) do
+    globals[i] = v
+end
+
+storage.connections = connections
--- loader.lua
+++ loader.lua
@@ -1,2 +1,20 @@
 -- Tohru~ (トール) 💖#0001
 
+if isfile('TohruAdmin/libs/import.lua') then
+    local generator = loadstring(readfile('TohruAdmin/libs/import.lua'), '@import.lua')
+    local import = generator('TohruAdmin', 'libs/import.lua')
+    return import('init')
+end
+
+local url = 'https://github.com/TohruMKDM/Tohru-Admin/archive/refs/heads/master.zip'
+local sub = string.sub
+local unzip = loadstring(game:HttpGet('https://raw.githubusercontent.com/TohruMKDM/Tohru-Admin/master/libs/unzip.lua'), '@unzip.lua')()
+local stream = unzip.newStream(game:HttpGet(url))
+
+for name, content in unzip.getFiles(stream, true) do
+    if sub(name, -1) == '/' then
+        makefolder(name)
+    else
+        writefile(name, content)
+    end
+end
