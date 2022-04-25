0223a16d9ef296df85de36827621f86ff81adee7
Tohru~ (トール) <admin@ikaros.pw>
2022-04-25 06:26:20 -0400

    push

--- misc/loader.lua
+++ misc/loader.lua
@@ -89,11 +89,7 @@ local launchScript
 launchScript = function()
     if import then
         notify('Tohru Admin', 'Tohru admin is already running', 'Restart?', 'Reload?', function(response)
-            local storage = import('storage')
-            for _, v in ipairs(storage.connections) do
-                v:Disconnect()
-            end
-            storage.gui:Destroy()
+            pcall(cleanUp)
             getgenv().import = nil
             if response == 'Restart?' then
                 launchScript()
